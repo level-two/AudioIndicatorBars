02ba890599e7293a50909c8fa53e814441a58417
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2019-03-25 14:08:15 +0300

    Revert "Revert "Fixed animation issues""
    This reverts commit b6d49d5826fbdfb12781659ec536f9ad00ab6952.

--- Sources/AudioIndicatorBarsView.swift
+++ Sources/AudioIndicatorBarsView.swift
@@ -9,109 +9,43 @@
 import Foundation
 import UIKit
 
-@IBDesignable
 open class AudioIndicatorBarsView: UIView {
-    
-    // MARK: - Lets
-    static let barsOffset: CGFloat = 0.5
-    
-    // MARK: - Vars
-    fileprivate var barsSet: [BarView] = []
-    
-    fileprivate var barCornerRadius: CGFloat = 0
-    
-    @IBInspectable
-    public var corner : CGFloat = 0 {
-        didSet {
-            self.barCornerRadius = corner
+    public init(
+        _ frame: CGRect,
+        _ barsCount: Int = 4,
+        _ barsCornerRadius: CGFloat = 0.0,
+        _ relativeBarWidth : CGFloat = 0.5,
+        _ color: UIColor = UIColor.white) {
+        
+        super.init(frame: frame)
+        
+        let sectionsWidth = bounds.width / CGFloat(barsCount)
+        for i in 0 ..< barsCount {
+            let barFrame =
+                CGRect(x: sectionsWidth * CGFloat(i), y: 0, width: sectionsWidth, height: frame.height)
+                    .insetBy(dx: sectionsWidth*(1-relativeBarWidth)/2, dy: 0)
+            
+            let bar = BarView(barFrame, barsCornerRadius, color, frame.height*0.2, frame.height)
+            self.barsSet.append(bar)
+            self.addSubview(bar)
         }
     }
     
-    fileprivate var barsCount: Int = 4
-    
-    @IBInspectable
-    public var bars : CGFloat = 4 {
-        didSet {
-            self.barsCount = Int(bars)
-        }
-    }
-    
-    
-    fileprivate var barColor: UIColor = UIColor.black
-    
-    @IBInspectable
-    public var color : UIColor = UIColor.black {
-        didSet {
-            self.barColor = color
-        }
-    }
-    
-    // MARK: - Initializers
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        self.doDraw(frame, self.barsCount, CGFloat(self.barCornerRadius), self.color)
     }
     
-    // Custom initializers
-    public init(
-        _ rect: CGRect,
-        _ barsCount: Int = 4,
-        _ barCornerRadius: CGFloat = 0.0,
-        _ color: UIColor = UIColor.black
-        ) {
-        self.barsCount = barsCount
-        self.barCornerRadius = barCornerRadius
-        self.color = color
-        super.init(frame: rect)
-
-        self.doDraw(rect, barsCount, barCornerRadius, color)
+    open func startAnimation() {
+        for bar in self.barsSet { bar.startAnimation() }
     }
     
-    // MARK: - Functions
-    override open func draw(_ rect: CGRect) {
-        super.draw(rect)
-        self.doDraw(rect, self.barsCount, CGFloat(self.barCornerRadius), self.color)
+    open func stopAnimation() {
+        for bar in self.barsSet { bar.stopAnimation() }
     }
     
-    // Do draw
-    func doDraw(
-        _ rect: CGRect,
-        _ barsCount: Int = 4,
-        _ cornerRadius: CGFloat = 0.0,
-        _ color: UIColor = UIColor.black
-        ) {
-        
-        let sectionsWidth = rect.width / CGFloat(self.barsCount)
-        for i in 0 ..< self.barsCount {
-            let x = sectionsWidth * CGFloat(i) + AudioIndicatorBarsView.barsOffset
-            let width = sectionsWidth - AudioIndicatorBarsView.barsOffset * 2
-            let y: CGFloat = rect.height - width
-            
-            let frame: CGRect = CGRect(x: x, y: y - AudioIndicatorBarsView.barsOffset, width: width, height: width)
-            let bar: BarView = BarView(frame, cornerRadius, color, x, width, rect.height)
-            
-            self.barsSet.append(bar)
-            
-            self.addSubview(bar)
-        }
-    }
-    
-    // Start animations
-    open func start() {
-        for bar in self.barsSet { bar.start() }
-    }
-    
-    // Stop animations
-    open func stop() {
-        for bar in self.barsSet { bar.stop() }
-    }
-    
-    deinit {
-        self.barsSet.removeAll()
-    }
-    
+    fileprivate var barsSet: [BarView] = []
 }
